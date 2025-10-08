cec971907154511260995d8eefcbc9b4f8f61b72
W I L L <will@gravina.dev>
2025-10-08 07:53:30 -0300

    chore: add prettier-plugin-terraform-formatter to enhance formatting support in project

--- configs/prettier-config/src/base.ts
+++ configs/prettier-config/src/base.ts
@@ -11,7 +11,13 @@ export const defineConfig = (config: Config = {}): Config => {
     semi: false,
     trailingComma: 'none',
     endOfLine: 'lf',
-    plugins: ['prettier-plugin-packagejson', 'prettier-plugin-prisma', 'prettier-plugin-tailwindcss', ...plugins],
+    plugins: [
+      'prettier-plugin-packagejson',
+      'prettier-plugin-prisma',
+      'prettier-plugin-terraform-formatter',
+      'prettier-plugin-tailwindcss',
+      ...plugins
+    ],
     printWidth: 120,
     ...rest
   }
